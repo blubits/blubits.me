565b9e5f525b5ff9ca4dbde2b5b3311cd352a6f3
Maded Batara III <maded@blubits.me>
2023-09-12 15:35:36 +0800

    fix: responsive

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,5 @@ import './src/styles/global.css'
 import '@fontsource/inter'                      // Inter regular
 import '@fontsource/inter/500.css'              // Inter semibold
 import '@fontsource/inter/700.css'              // Inter bold
+import '@fontsource/inter-tight'        // Inter Tight regular
 import '@fontsource/inter-tight/500.css'        // Inter Tight semibold
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,10 @@ import Header from './header.js'
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className="w-1/2 mx-auto mt-8 flex flex-col">
+    <div className="w-5/6 md:w-3/4 lg:w-1/2 mx-auto mt-8 flex flex-col">
       <Header />
       <main>  
-        <h1 className="text-4xl my-5 font-semibold font-display">{pageTitle}</h1>
+        <h1 className="text-4xl my-5 font-display font-semibold">{pageTitle}</h1>
         {children}
       </main>
     </div>
