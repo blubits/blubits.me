6439e97b49f11afb71b2f7f44ff83c81082fda3c
Maded Batara III <maded@blubits.me>
2023-09-13 15:20:05 +0800

    fix: adjusted margins

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Logo from '../assets/icon-small.svg'
 
 const Header = () => {
     return (
-        <nav className="flex flex-row my-4">
+        <nav className="flex flex-row my-2">
             <Link to="/">
                 <span className="sr-only">blubits.me</span>
                 <Logo className="h-12 w-12 p-2 stroke-white hover:bg-slate-900 hover:rounded-full" />
