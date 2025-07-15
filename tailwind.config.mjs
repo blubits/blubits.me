bdd28eb61c50e72a0223a6643fa54a79dfbe4526
Maded Batara III <dev@contact.blubits.me>
2025-07-15 22:52:15 +0800

    refactor: update fontsource dependencies and clean up imports

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,39 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
-const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter"],
-        sanstight: ["Inter"],
-        mono: ["JetBrains Mono"],
-      },
-      typography: {
+      typography: () => ({
         DEFAULT: {
           css: {
-            "code::before": {
-              content: '""',
-            },
-            "code::after": {
-              content: '""',
-            },
+            "code::before": { content: '""' },
+            "code::after": { content: '""' },
             "blockquote p:first-of-type::before": null,
             "blockquote p:last-of-type::after": null,
           },
         },
-      },
+      }),
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    plugin(function ({ addVariant }) {
-      addVariant(
-        "prose-inline-code",
-        '&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))',
-      );
-    }),
-  ],
 };
